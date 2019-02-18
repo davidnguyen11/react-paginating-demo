096741c15ed3f455ef882473c3c10f6fe0dde4ef
Nguyen <dzungnguyen179@gmail.com>
2019-02-18 17:04:51 +0900

    add build

--- next.config.js
+++ next.config.js
@@ -4,5 +4,5 @@ module.exports = {
       '/': { page: '/' }
     }
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/react-paginating-demo' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/react-paginating-demo/' : '',
 }
